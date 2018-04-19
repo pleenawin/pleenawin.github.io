2108741ce433cfb5f8e30372450028f62432b360
Paulina <pleena.win@gmail.com>
2018-04-19 11:29:14 -0700

    fixed carousel

--- fewd_final-project/js/carousel.js
+++ fewd_final-project/js/carousel.js
@@ -12,15 +12,15 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  var indicator = document.getElementsByClassName("circle");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+  for (i = 0; i < indicator.length; i++) {
+      indicator[i].className = indicator[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  indicator[slideIndex-1].className += " active";
 }
