8c25992e2b0c9f60a19308dbd8605c6e16750561
Paulina <pleena.win@gmail.com>
2018-04-19 15:35:25 -0700

    cleaned up, moved contact page

--- fewd_final-project/js/carousel.js
+++ fewd_final-project/js/carousel.js
@@ -1,5 +1,5 @@
 var slideIndex = 1;
-showSlides(slideIndex);
+  showSlides(slideIndex);
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -11,16 +11,16 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("slides");
-  var indicator = document.getElementsByClassName("control");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+  var slides = $(".slides");
+  var indicator = $(".control");
+  if (n > slides.length) {slideIndex = 1};    
+  if (n < 1) {slideIndex = slides.length};
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
-  }
+  };
   for (i = 0; i < indicator.length; i++) {
       indicator[i].className = indicator[i].className.replace(" active", "");
-  }
+  };
   slides[slideIndex-1].style.display = "block";  
   indicator[slideIndex-1].className += " active";
 }
--- fewd_final-project/js/index.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-	$("#navdrawer").click(function(){
-		$("#globalnav").slideToggle();
-	})
-})
